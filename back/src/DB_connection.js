a8227b703044a6d402d36027d77d6571c517f880
unknown <rocio.sosa94@gmail.com>
2024-09-23 17:52:14 -0300

    fixing details

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY} = process.env;
 const FavoriteModel = require ('./models/Favorite')
 const UserModel= require('./models/User');
 
@@ -9,8 +9,12 @@ const UserModel= require('./models/User');
 // Recuerda pasarle la información de tu archivo '.env'.
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+// const sequelize = new Sequelize(
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+//    { logging: false, native: false }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY,
    { logging: false, native: false }
 );
 
